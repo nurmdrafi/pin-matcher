f96b22ffcbf35a0cb1cebb6ac8551de23a15bbcc
nurmdrafi <nurmdrafi2014@gmail.com>
2022-02-16 19:35:20 +0600

    matching condition updated

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Generate Pin
 document.getElementById("get-pin").addEventListener("click", function () {
   const pinValue = getPin();
-  let pinOutput = document.getElementById("pin-output");
-  console.log(pinValue);
-  pinOutput.value = pinValue;
+  let showPin = document.getElementById("show-pin");
+  showPin.value = pinValue;
+  document.getElementById("notify-failed").style.display = "none";
+  document.getElementById("notify-success").style.display = "none";
 });
+
 function getPin() {
   const pin = Math.round(Math.random() * 10000);
   if (pin.toString().length < 4) {
@@ -39,3 +41,26 @@ document.getElementById("keypad").addEventListener("click", function (e) {
 });
 
 // Matching
+document.getElementById("submit-btn").addEventListener("click", function () {
+  const pin = document.getElementById("show-pin");
+  const pinValue = pin.value;
+  const numberInput = document.getElementById("number-input");
+  let numberInputValue = numberInput.value;
+  const notifyFailed = document.getElementById("notify-failed");
+  const notifySuccess = document.getElementById("notify-success");
+
+  if (
+    pinValue != numberInputValue ||
+    pinValue == "" ||
+    numberInputValue == ""
+  ) {
+    notifyFailed.style.display = "block";
+    notifySuccess.style.display = "none";
+  } else if (pinValue == numberInputValue) {
+    notifyFailed.style.display = "none";
+    notifySuccess.style.display = "block";
+  }
+  pin.value = "";
+  numberInput.value = "";
+});
+console.log(count);
